23ca75aae6fe0a90adeea218f9c74ca436066802
mandipdhakal <m.dhakal@hydroneo.net>
2025-10-28 13:18:33 +0700

    Initial commit

---
+++ src/main/java/com/example/rest_vs_graph/RestVsGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_vs_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVsGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVsGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rest_vs_graph/RestVsGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_vs_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVsGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
